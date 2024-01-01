8dd53d17369504d085536e68d21b97f0e436ab36
Miruna Muj <miruna.muj@yahoo.com>
2024-01-01 15:01:59 +0200

    Add project files.

---
+++ Proiect_Muj_Miruna_Luana/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proiect_Muj_Miruna_Luana.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
